0f0ac97640dbe18e6a082932841f28d8e952db7e
aosipenko <aoskiev@gmail.com>
2026-01-12 20:06:11 +0100

    Session 1

---
+++ src/main/java/org/prog/session1/Main.java
@@ -0,0 +1,20 @@
+package org.prog.session1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        int i = 10;
+        System.out.println(i);
+        i = i - 3;
+        System.out.println(i);
+        i = i + 21;
+        System.out.println(i);
+        System.out.println(5 * 5);
+        System.out.println(21 / 3);
+        System.out.println(" 5 *sdkfjs;d 5   ");
+
+    }
+}
+
