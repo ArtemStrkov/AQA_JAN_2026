802ec42720ccb679403bea04247efcc37511d169
Artem <artemstrukov2005@gmail.com>
2026-01-27 21:16:20 +0100

    Homework 3

---
+++ src/main/java/org/prog/session3/HomeWork3.java
@@ -0,0 +1,52 @@
+package org.prog.session3;
+
+//TODO: Write bubble sort
+//TODO: Fill array with random numbers *
+
+public class HomeWork3 {
+
+    public static void main(String[] args) {
+
+
+        //4,1,8,5,8,0,12,9
+        //1,4,5,8,0,8,9,12
+        //1,4,5,0,8,8,9,12
+        //1,4,0,5,8,8,9,12
+        //1,0,4,5,8,8,9,12
+        //0,1,4,5,8,8,9,12
+        //0,1,4,5,8,8,9,12 < DONE
+
+        int[] myArrayToSort = new int[] {2, 5, 9, 3, 6, 1, 4, 7, 8, 0};
+        boolean condition = true;
+
+
+        while (condition) {
+            condition = false;
+
+            for (int i = 0; i < myArrayToSort.length - 1; i++) {
+                //if current element > next element > switch places
+                if (myArrayToSort[i] > myArrayToSort[i + 1]) {
+
+                    int current = myArrayToSort[i];
+                    myArrayToSort[i] = myArrayToSort[i + 1];
+                    myArrayToSort[i + 1] = current;
+
+                     condition = true;
+                }
+
+                //if switch happened -> condition = true
+                //if no switch happened -> condition = false -> end sorting
+
+
+            }
+        }
+
+        for (int i = 0; i < myArrayToSort.length; i++)
+        {
+            System.out.println(myArrayToSort[i]);
+        }
+
+
+    }
+
+}
