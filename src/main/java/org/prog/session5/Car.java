77016be06048fbf30969958b2680874bed0d12c5
Artem <artemstrukov2005@gmail.com>
2026-02-05 20:52:25 +0100

    Homework 5

--- src/main/java/org/prog/session5/Car.java
+++ src/main/java/org/prog/session5/Car.java
@@ -2,22 +2,22 @@ package org.prog.session5;
 
 public class Car {
 
-    //TODO: add owner as String
-    //TODO: make goTo print "car owned by {owner} is going to {destination} at {speed}
-    //TODO: do not forget to set owner name :)
+    public String owner;
+    public String destination;
+    public int speed;
 
-    public String color;
-    public int mileage;
-    public String plateNumber;
+    public void goTo(String owner, String destination, int speed)
+    {
 
-    public void goTo(String destination, int speed) {
-        System.out.println("before trip miles: " + mileage);
-        if ("black".equals(color)) {
-            System.out.println("Black cars are cool");
-        }
-        mileage += speed * 10;
-        System.out.println(color + " car is going to " + destination +
-                " at " + speed + " km/h");
-        System.out.println("After trip miles: " + mileage);
+        System.out.println("car owned by " + owner + " is going to " + destination + " at " + speed + " km/h");
+
+//        System.out.println("before trip miles: " + mileage);
+//        if ("black".equals(color)) {
+//            System.out.println("Black cars are cool");
+//        }
+//        mileage += speed * 10;
+//        System.out.println(color + " car is going to " + destination +
+//                " at " + speed + " km/h");
+//        System.out.println("After trip miles: " + mileage);
     }
 }
---
+++ src/main/java/org/prog/session5/Homework5.java
@@ -0,0 +1,15 @@
+package org.prog.session5;
+
+//TODO: add owner as String
+//TODO: make goTo print "car owned by {owner} is going to {destination} at {speed}
+//TODO: do not forget to set owner name :)
+
+public class Homework5 {
+
+    public static void main(String[] args)
+    {
+       Car MyCar = new Car();
+
+       MyCar.goTo("Artem", "Lviv", 75);
+    }
+}
